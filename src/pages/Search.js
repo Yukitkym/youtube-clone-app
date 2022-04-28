495cb72c6f322c2e2b3eba564aaeee23c239efc0
YukiTakayama <yukitkym1011@gmail.com>
2022-04-28 14:45:36 +0900

    Create youtube-clone-app

---
+++ src/pages/Search.js
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect } from 'react';
+import { useLocation} from 'react-router-dom';
+
+import { fetchSearchData } from '../apis';
+import Layout from '../components/Layout/Layout';
+import { Store } from '../store/index';
+import VideoGrid from '../components/VideoGrid/VideoGrid';
+import VideoGridItem from '../components/VideoGridItem/VideoGridItem';
+
+const Search = () => {
+  const { globalState, setGlobalState } = useContext(Store);
+  const location = useLocation();
+  const setSearchResult = async () => {
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get('query');
+    if (query) {
+      await fetchSearchData(query).then((res) => {
+        setGlobalState({type: 'SET_SEARCHED', payload: {searched: res.data.items}})
+      })
+    }
+  }
+  useEffect(() => {
+    setSearchResult();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.search])
+  return (
+    <Layout>
+        <VideoGrid>
+          {
+            globalState.searched ? globalState.searched.map((search) => {
+              return (
+                <VideoGridItem
+                  id={search.id.videoId}
+                  key={search.id.videoId}
+                  src={search.snippet.thumbnails.high.url || search.snippet.thumbnails.standard.url}
+                  title={search.snippet.title}
+                />
+              )
+            }) : <span>no data</span>
+          }
+        </VideoGrid>
+    </Layout>
+  );
+};
+
+export default Search;
